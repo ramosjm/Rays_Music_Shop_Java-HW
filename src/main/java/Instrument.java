e7f3eb2c458cc1af6d6f43db62a9eda3ecac955e
ramosjm <jjramos_2004@hotmail.com>
2019-04-06 18:05:50 +0100

    added constructor to instrument class.

--- src/main/java/Instrument.java
+++ src/main/java/Instrument.java
@@ -4,4 +4,10 @@ public abstract class Instrument {
     private double sellPrice;
     private InstrumentType instrumentType;
 
+    public Instrument(double buyPrice, double sellPrice, InstrumentType instrumentType){
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        this.instrumentType = instrumentType;
+    }
+
 }
