6c1b672dd004aaf1a2844a5ec4621b94b6316905
ramosjm <jjramos_2004@hotmail.com>
2019-04-06 18:12:37 +0100

    added guitar class.

---
+++ src/main/java/Guitar.java
@@ -0,0 +1,9 @@
+public class Guitar extends Instrument {
+
+    private InstrumentType instrumentType;
+
+    public Guitar(double buyPrice, double sellPrice, InstrumentType instrumentType){
+        super(buyPrice,sellPrice,instrumentType);
+    }
+
+}
